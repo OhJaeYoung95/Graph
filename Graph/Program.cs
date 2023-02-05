7d9632f4400dd1a07086d465dad9e5938d302cea
OhJaeYoung <ostyoung@naver.com>
2023-02-05 12:32:08 +0900

    DFS

--- Graph/Program.cs
+++ Graph/Program.cs
@@ -12,8 +12,8 @@ namespace Graph
                 { 0,1,0,1,0,0},
                 { 1,0,1,1,0,0},
                 { 0,1,0,0,0,0},
-                { 1,1,0,0,1,0},
-                { 0,0,0,1,0,1},
+                { 1,1,0,0,0,0},
+                { 0,0,0,0,0,1},
                 { 0,0,0,0,1,0},
             };
 
@@ -22,16 +22,71 @@ namespace Graph
                 new List<int>() { 1, 3 },
                 new List<int>() { 0, 2, 3 },
                 new List<int>() { 1 },
-                new List<int>() { 0, 1, 4 },
-                new List<int>() { 3, 5 },
+                new List<int>() { 0, 1 },
+                new List<int>() { 5 },
                 new List<int>() { 4 },
             };
+
+            public bool[] visited = new bool[6];
+            // 1) 우선 now부터 방문하고, 
+            // 2) now와 연결된 정점들을 하나씩 확인해서, [아직 미발견(미방문) 상태라면] 방문한다.
+            public void DFS(int now)
+            {
+                Console.WriteLine(now);
+                visited[now] = true; // 1) 우선 now부터 방문하고,
+
+                for(int next = 0; next < 6; next++)
+                {
+                    if (adj[now, next] == 0)    // 연결되어 있지 않으면 스킵
+                        continue;
+                    if (visited[next])          // 이미 방문했으면 스킵
+                        continue;
+                    DFS(next);
+                }
+            }
+
+            public void DFS2(int now)
+            {
+                Console.WriteLine(now);
+                visited[now] = true; // 1) 우선 now부터 방문하고,
+                
+                foreach(int next in adj2[now])
+                {
+                    if (visited[next])          // 이미 방문했으면 스킵
+                        continue;
+                    DFS2(next);
+                }
+            }
+
+            public void DfsNDfs2()
+            {
+                Console.WriteLine("DFS");
+                DFS(0);
+                visited = new bool[6];
+                Console.WriteLine("DFS2");
+                DFS2(1);
+
+            }
+
+            public void SearchAll()
+            {
+                Console.WriteLine("SearchAll");
+                visited = new bool[6];
+                for (int now = 0; now < 6; now++)
+                    if (visited[now] == false)
+                        DFS(now);
+            }
         }
 
         static void Main(string[] args)
         {
             // DFS (Depth First Search 깊이 우선 탐색)
+            Graph graph = new Graph();
+            graph.DfsNDfs2();
+            graph.SearchAll();
+
             // BFS (Breadth First Search 너비 우선 탐색)
+
         }
     }
 }
